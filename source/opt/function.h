e9a81ffe9c5afd09d8072d7d759f75ec65bd97a9
Victor Lomuller <victor@codeplay.com>
2017-11-27 18:49:11 +0000

    Fix silly infinit recursion

--- source/opt/function.h
+++ source/opt/function.h
@@ -82,8 +82,8 @@ class Function {
 
   iterator begin() { return iterator(&blocks_, blocks_.begin()); }
   iterator end() { return iterator(&blocks_, blocks_.end()); }
-  const_iterator begin() const { return begin(); }
-  const_iterator end() const { return end(); }
+  const_iterator begin() const { return cbegin(); }
+  const_iterator end() const { return cend(); }
   const_iterator cbegin() const {
     return const_iterator(&blocks_, blocks_.cbegin());
   }
