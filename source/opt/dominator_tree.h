7e3e97caf3a3ba76edf0e6239460adc989f194ee
Victor Lomuller <victor@codeplay.com>
2017-12-05 09:49:52 +0000

    Revert "Add custom iterator to iterate over a tree in a DFS order"
    This reverts commit 44c46f6298fd0dd1bc150614b83c0c59ba261af3.

--- source/opt/dominator_tree.h
+++ source/opt/dominator_tree.h
@@ -17,8 +17,6 @@
 
 #include <cstdint>
 #include <map>
-#include <stack>
-#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -38,16 +36,6 @@ struct DominatorTreeNode {
         dfs_num_pre_(-1),
         dfs_num_post_(-1) {}
 
-  using iterator = std::vector<DominatorTreeNode*>::iterator;
-  using const_iterator = std::vector<DominatorTreeNode*>::const_iterator;
-
-  iterator begin() { return children_.begin(); }
-  iterator end() { return children_.end(); }
-  const_iterator begin() const { return cbegin(); }
-  const_iterator end() const { return cend(); }
-  const_iterator cbegin() const { return children_.begin(); }
-  const_iterator cend() const { return children_.end(); }
-
   inline uint32_t id() const { return bb_->id(); }
 
   ir::BasicBlock* bb_;
@@ -62,101 +50,14 @@ struct DominatorTreeNode {
   int dfs_num_post_;
 };
 
-// Helper class to iterate over a tree in a DFS order.
-// It work by manipulating the node children begin/end iterators.
-template <typename NodeTy>
-class TreeDFSIterator {
-  // Type alias to keep track of the const qualifier.
-  using NodeIterator =
-      typename std::conditional<std::is_const<NodeTy>::value,
-                                typename NodeTy::const_iterator,
-                                typename NodeTy::iterator>::type;
-
- public:
-  // Standard iterator interface.
-  using reference = typename NodeIterator::reference;
-  using value_type = typename NodeIterator::value_type;
-
-  static inline TreeDFSIterator begin(NodeIterator node_it,
-                                      NodeIterator end_it) {
-    return TreeDFSIterator(node_it, end_it);
-  }
-
-  static inline TreeDFSIterator end(NodeIterator end_it) {
-    return TreeDFSIterator(end_it);
-  }
-
-  bool operator==(const TreeDFSIterator& x) const {
-    return current_.first == x.current_.first;
-  }
-
-  bool operator!=(const TreeDFSIterator& x) const { return !(*this == x); }
-
-  reference operator*() const { return *current_.first; }
-
-  TreeDFSIterator& operator++() {
-    MoveToNextNode();
-    return *this;
-  }
-
-  TreeDFSIterator operator++(int) {
-    TreeDFSIterator tmp = *this;
-    ++*this;
-    return tmp;
-  }
-
- private:
-  inline TreeDFSIterator(NodeIterator node_it, NodeIterator end_it)
-      : current_(node_it, end_it) {
-    if (node_it != end_it) {
-      // Force the iterator to be identical with end().
-      parent_iterators_.push(std::make_pair(end_it, end_it));
-    }
-  }
-
-  // end iterator.
-  explicit inline TreeDFSIterator(NodeIterator end_it)
-      : TreeDFSIterator(end_it, end_it) {}
-
-  // Set the first parent in the as the current iterator.
-  // Do nothing if the stack is empty.
-  inline void pop_parent() {
-    if (!parent_iterators_.empty()) {
-      current_ = parent_iterators_.top();
-      parent_iterators_.pop();
-    }
-  }
-
-  // Move the iterator to the next node in the tree.
-  // if we are at the end, do nothing, otherwise
-  // if our current node has children, use the children iterator and push the
-  // current into the stack.
-  // If we reach the end of the local iterator, pop the parent one.
-  inline void MoveToNextNode() {
-    if (current_.first == current_.second && parent_iterators_.empty()) return;
-    assert(current_.first != current_.second);
-    value_type current_node = *current_.first;
-    current_.first++;
-    // if there is remaining nodes, save them for latter
-    if (current_.first != current_.second) parent_iterators_.emplace(current_);
-    if (current_node->begin() != current_node->end())
-      current_ = {current_node->begin(), current_node->end()};
-    else
-      pop_parent();
-  }
-
-  std::pair<NodeIterator, NodeIterator> current_;
-  std::stack<std::pair<NodeIterator, NodeIterator>> parent_iterators_;
-};
-
 // A class representing a tree of BasicBlocks in a given function, where each
 // node is dominated by its parent.
 class DominatorTree {
  public:
   // Map OpLabel ids to dominator tree nodes
   using DominatorTreeNodeMap = std::map<uint32_t, DominatorTreeNode>;
-  using iterator = TreeDFSIterator<DominatorTreeNode>;
-  using const_iterator = TreeDFSIterator<const DominatorTreeNode>;
+  using iterator = DominatorTreeNodeMap::iterator;
+  using const_iterator = DominatorTreeNodeMap::const_iterator;
 
   // List of DominatorTreeNode to define the list of roots
   using DominatorTreeNodeList = std::vector<DominatorTreeNode*>;
@@ -166,14 +67,12 @@ class DominatorTree {
   DominatorTree() : postdominator_(false) {}
   explicit DominatorTree(bool post) : postdominator_(post) {}
 
-  iterator begin() { return iterator::begin(roots_.begin(), roots_.end()); }
-  iterator end() { return iterator::end(roots_.end()); }
+  iterator begin() { return nodes_.begin(); }
+  iterator end() { return nodes_.end(); }
   const_iterator begin() const { return cbegin(); }
   const_iterator end() const { return cend(); }
-  const_iterator cbegin() const {
-    return const_iterator::begin(roots_.begin(), roots_.end());
-  }
-  const_iterator cend() const { return const_iterator::end(roots_.end()); }
+  const_iterator cbegin() const { return nodes_.begin(); }
+  const_iterator cend() const { return nodes_.end(); }
 
   roots_iterator roots_begin() { return roots_.begin(); }
   roots_iterator roots_end() { return roots_.end(); }
@@ -245,19 +144,29 @@ class DominatorTree {
 
   // Applies the std::function |func| to all nodes in the dominator tree.
   bool Visit(std::function<bool(DominatorTreeNode*)> func) {
-    for (auto n : *this) {
-      if (!func(n)) return false;
+    for (auto n : roots_) {
+      if (!Visit(n, func)) return false;
     }
     return true;
   }
 
   // Applies the std::function |func| to all nodes in the dominator tree.
   bool Visit(std::function<bool(const DominatorTreeNode*)> func) const {
-    for (auto n : *this) {
-      if (!func(n)) return false;
+    for (auto n : roots_) {
+      if (!Visit(n, func)) return false;
     }
     return true;
   }
+
+  // Applies the std::function |func| to |node| then applies it to nodes
+  // children.
+  bool Visit(DominatorTreeNode* node,
+             std::function<bool(DominatorTreeNode*)> func);
+
+  // Applies the std::function |func| to |node| then applies it to nodes
+  // children.
+  bool Visit(const DominatorTreeNode* node,
+             std::function<bool(const DominatorTreeNode*)> func) const;
 
  private:
   // Adds the basic block |bb| to the tree structure if it doesn't already
