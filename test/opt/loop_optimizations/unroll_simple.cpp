97b5069f5e144b6fa7b199a754fec81f6b0e35e7
Stephen McGroarty <stephen@codeplay.com>
2018-01-17 19:50:53 +0000

    Made test into an actual check

--- test/opt/loop_optimizations/unroll_simple.cpp
+++ test/opt/loop_optimizations/unroll_simple.cpp
@@ -38,121 +38,232 @@ layout(location = 0) out vec4 c;
 void main() {
   float x[10];
   for (int i = 0; i < 10; ++i) {
-    x[0] = 1.0f;
+    x[i] = 1.0f;
   }
 }
 */
 TEST_F(PassClassTest, BasicVisitFromEntryPoint) {
-  // Unoptimised.
-  /*  const std::string text = R"(
-               OpCapability Shader
-          %1 = OpExtInstImport "GLSL.std.450"
-               OpMemoryModel Logical GLSL450
-               OpEntryPoint Fragment %4 "main" %33
-               OpExecutionMode %4 OriginUpperLeft
-               OpSource GLSL 330
-               OpName %4 "main"
-               OpName %8 "i"
-               OpName %24 "x"
-               OpName %33 "c"
-               OpDecorate %33 Location 0
-          %2 = OpTypeVoid
-          %3 = OpTypeFunction %2
-          %6 = OpTypeInt 32 1
-          %7 = OpTypePointer Function %6
-          %9 = OpConstant %6 0
-         %16 = OpConstant %6 10
-         %17 = OpTypeBool
-         %19 = OpTypeFloat 32
-         %20 = OpTypeInt 32 0
-         %21 = OpConstant %20 10
-         %22 = OpTypeArray %19 %21
-         %23 = OpTypePointer Function %22
-         %25 = OpConstant %19 1
-         %26 = OpTypePointer Function %19
-         %29 = OpConstant %6 1
-         %31 = OpTypeVector %19 4
-         %32 = OpTypePointer Output %31
-         %33 = OpVariable %32 Output
-          %4 = OpFunction %2 None %3
-          %5 = OpLabel
-          %8 = OpVariable %7 Function
-         %24 = OpVariable %23 Function
-               OpStore %8 %9
-               OpBranch %10
-         %10 = OpLabel
-               OpLoopMerge %12 %13 None
-               OpBranch %14
-         %14 = OpLabel
-         %15 = OpLoad %6 %8
-         %18 = OpSLessThan %17 %15 %16
-               OpBranchConditional %18 %11 %12
-         %11 = OpLabel
-         %27 = OpAccessChain %26 %24 %9
-               OpStore %27 %25
-               OpBranch %13
-         %13 = OpLabel
-         %28 = OpLoad %6 %8
-         %30 = OpIAdd %6 %28 %29
-               OpStore %8 %30
-               OpBranch %10
-         %12 = OpLabel
-               OpReturn
-               OpFunctionEnd
-  )";*/
-
+  // clang-format off
   // With opt::LocalMultiStoreElimPass
   const std::string text = R"(
-         OpCapability Shader
-         %1 = OpExtInstImport "GLSL.std.450"
-         OpMemoryModel Logical GLSL450
-         OpEntryPoint Fragment %2 "main" %3
-         OpExecutionMode %2 OriginUpperLeft
-         OpSource GLSL 330
-         OpName %2 "main"
-         OpName %5 "x"
-         OpName %3 "c"
-         OpDecorate %3 Location 0
-         %6 = OpTypeVoid
-         %7 = OpTypeFunction %6
-         %8 = OpTypeInt 32 1
-         %9 = OpTypePointer Function %8
-         %10 = OpConstant %8 0
-         %11 = OpConstant %8 10
-         %12 = OpTypeBool
-         %13 = OpTypeFloat 32
-         %14 = OpTypeInt 32 0
-         %15 = OpConstant %14 10
-         %16 = OpTypeArray %13 %15
-         %17 = OpTypePointer Function %16
-         %18 = OpConstant %13 1
-         %19 = OpTypePointer Function %13
-         %20 = OpConstant %8 1
-         %21 = OpTypeVector %13 4
-         %22 = OpTypePointer Output %21
-         %3 = OpVariable %22 Output
-         %2 = OpFunction %6 None %7
-         %23 = OpLabel
-         %5 = OpVariable %17 Function
-         OpBranch %24
-         %24 = OpLabel
-         %34 = OpPhi %8 %10 %23 %33 %26
-         OpLoopMerge %25 %26 Unroll
-         OpBranch %27
-         %27 = OpLabel
-         %29 = OpSLessThan %12 %34 %11
-         OpBranchConditional %29 %30 %25
-         %30 = OpLabel
-         %31 = OpAccessChain %19 %5 %10
-         OpStore %31 %18
-         OpBranch %26
-         %26 = OpLabel
-         %33 = OpIAdd %8 %34 %20
-         OpBranch %24
-         %25 = OpLabel
-         OpReturn
-         OpFunctionEnd
+            OpCapability Shader
+            %1 = OpExtInstImport "GLSL.std.450"
+            OpMemoryModel Logical GLSL450
+            OpEntryPoint Fragment %2 "main" %3
+            OpExecutionMode %2 OriginUpperLeft
+            OpSource GLSL 330
+            OpName %2 "main"
+            OpName %5 "x"
+            OpName %3 "c"
+            OpDecorate %3 Location 0
+            %6 = OpTypeVoid
+            %7 = OpTypeFunction %6
+            %8 = OpTypeInt 32 1
+            %9 = OpTypePointer Function %8
+            %10 = OpConstant %8 0
+            %11 = OpConstant %8 10
+            %12 = OpTypeBool
+            %13 = OpTypeFloat 32
+            %14 = OpTypeInt 32 0
+            %15 = OpConstant %14 10
+            %16 = OpTypeArray %13 %15
+            %17 = OpTypePointer Function %16
+            %18 = OpConstant %13 1
+            %19 = OpTypePointer Function %13
+            %20 = OpConstant %8 1
+            %21 = OpTypeVector %13 4
+            %22 = OpTypePointer Output %21
+            %3 = OpVariable %22 Output
+            %2 = OpFunction %6 None %7
+            %23 = OpLabel
+            %5 = OpVariable %17 Function
+            OpBranch %24
+            %24 = OpLabel
+            %35 = OpPhi %8 %10 %23 %34 %26
+            OpLoopMerge %25 %26 Unroll
+            OpBranch %27
+            %27 = OpLabel
+            %29 = OpSLessThan %12 %35 %11
+            OpBranchConditional %29 %30 %25
+            %30 = OpLabel
+            %32 = OpAccessChain %19 %5 %35
+            OpStore %32 %18
+            OpBranch %26
+            %26 = OpLabel
+            %34 = OpIAdd %8 %35 %20
+            OpBranch %24
+            %25 = OpLabel
+            OpReturn
+            OpFunctionEnd
   )";
+
+  const std::string output = 
+R"(OpCapability Shader
+%1 = OpExtInstImport "GLSL.std.450"
+OpMemoryModel Logical GLSL450
+OpEntryPoint Fragment %2 "main" %3
+OpExecutionMode %2 OriginUpperLeft
+OpSource GLSL 330
+OpName %2 "main"
+OpName %4 "x"
+OpName %3 "c"
+OpDecorate %3 Location 0
+%5 = OpTypeVoid
+%6 = OpTypeFunction %5
+%7 = OpTypeInt 32 1
+%8 = OpTypePointer Function %7
+%9 = OpConstant %7 0
+%10 = OpConstant %7 10
+%11 = OpTypeBool
+%12 = OpTypeFloat 32
+%13 = OpTypeInt 32 0
+%14 = OpConstant %13 10
+%15 = OpTypeArray %12 %14
+%16 = OpTypePointer Function %15
+%17 = OpConstant %12 1
+%18 = OpTypePointer Function %12
+%19 = OpConstant %7 1
+%20 = OpTypeVector %12 4
+%21 = OpTypePointer Output %20
+%3 = OpVariable %21 Output
+%2 = OpFunction %5 None %6
+%22 = OpLabel
+%4 = OpVariable %16 Function
+OpBranch %23
+%23 = OpLabel
+%24 = OpPhi %7 %9 %22 %103 %102
+OpBranch %28
+%28 = OpLabel
+%29 = OpSLessThan %11 %24 %10
+OpBranch %30
+%30 = OpLabel
+%31 = OpAccessChain %18 %4 %24
+OpStore %31 %17
+OpBranch %26
+%26 = OpLabel
+%25 = OpIAdd %7 %24 %19
+OpBranch %32
+%32 = OpLabel
+%33 = OpPhi %7 %9 %22 %39 %38
+OpBranch %34
+%34 = OpLabel
+%35 = OpSLessThan %11 %25 %10
+OpBranch %36
+%36 = OpLabel
+%37 = OpAccessChain %18 %4 %25
+OpStore %37 %17
+OpBranch %38
+%38 = OpLabel
+%39 = OpIAdd %7 %25 %19
+OpBranch %40
+%40 = OpLabel
+%41 = OpPhi %7 %9 %22 %47 %46
+OpBranch %42
+%42 = OpLabel
+%43 = OpSLessThan %11 %39 %10
+OpBranch %44
+%44 = OpLabel
+%45 = OpAccessChain %18 %4 %39
+OpStore %45 %17
+OpBranch %46
+%46 = OpLabel
+%47 = OpIAdd %7 %39 %19
+OpBranch %48
+%48 = OpLabel
+%49 = OpPhi %7 %9 %22 %55 %54
+OpBranch %50
+%50 = OpLabel
+%51 = OpSLessThan %11 %47 %10
+OpBranch %52
+%52 = OpLabel
+%53 = OpAccessChain %18 %4 %47
+OpStore %53 %17
+OpBranch %54
+%54 = OpLabel
+%55 = OpIAdd %7 %47 %19
+OpBranch %56
+%56 = OpLabel
+%57 = OpPhi %7 %9 %22 %63 %62
+OpBranch %58
+%58 = OpLabel
+%59 = OpSLessThan %11 %55 %10
+OpBranch %60
+%60 = OpLabel
+%61 = OpAccessChain %18 %4 %55
+OpStore %61 %17
+OpBranch %62
+%62 = OpLabel
+%63 = OpIAdd %7 %55 %19
+OpBranch %64
+%64 = OpLabel
+%65 = OpPhi %7 %9 %22 %71 %70
+OpBranch %66
+%66 = OpLabel
+%67 = OpSLessThan %11 %63 %10
+OpBranch %68
+%68 = OpLabel
+%69 = OpAccessChain %18 %4 %63
+OpStore %69 %17
+OpBranch %70
+%70 = OpLabel
+%71 = OpIAdd %7 %63 %19
+OpBranch %72
+%72 = OpLabel
+%73 = OpPhi %7 %9 %22 %79 %78
+OpBranch %74
+%74 = OpLabel
+%75 = OpSLessThan %11 %71 %10
+OpBranch %76
+%76 = OpLabel
+%77 = OpAccessChain %18 %4 %71
+OpStore %77 %17
+OpBranch %78
+%78 = OpLabel
+%79 = OpIAdd %7 %71 %19
+OpBranch %80
+%80 = OpLabel
+%81 = OpPhi %7 %9 %22 %87 %86
+OpBranch %82
+%82 = OpLabel
+%83 = OpSLessThan %11 %79 %10
+OpBranch %84
+%84 = OpLabel
+%85 = OpAccessChain %18 %4 %79
+OpStore %85 %17
+OpBranch %86
+%86 = OpLabel
+%87 = OpIAdd %7 %79 %19
+OpBranch %88
+%88 = OpLabel
+%89 = OpPhi %7 %9 %22 %95 %94
+OpBranch %90
+%90 = OpLabel
+%91 = OpSLessThan %11 %87 %10
+OpBranch %92
+%92 = OpLabel
+%93 = OpAccessChain %18 %4 %87
+OpStore %93 %17
+OpBranch %94
+%94 = OpLabel
+%95 = OpIAdd %7 %87 %19
+OpBranch %96
+%96 = OpLabel
+%97 = OpPhi %7 %9 %22 %103 %102
+OpBranch %98
+%98 = OpLabel
+%99 = OpSLessThan %11 %95 %10
+OpBranch %100
+%100 = OpLabel
+%101 = OpAccessChain %18 %4 %95
+OpStore %101 %17
+OpBranch %102
+%102 = OpLabel
+%103 = OpIAdd %7 %95 %19
+OpBranch %27
+%27 = OpLabel
+OpReturn
+OpFunctionEnd
+)";
   // clang-format on
   std::unique_ptr<ir::IRContext> context =
       BuildModule(SPV_ENV_UNIVERSAL_1_1, nullptr, text,
@@ -163,8 +274,7 @@ TEST_F(PassClassTest, BasicVisitFromEntryPoint) {
 
   opt::LoopUnroller loop_unroller;
   SetDisassembleOptions(SPV_BINARY_TO_TEXT_OPTION_NO_HEADER);
-  std::cout << std::get<0>(
-      SinglePassRunAndDisassemble<opt::LoopUnroller>(text, false, true));
+  SinglePassRunAndCheck<opt::LoopUnroller>(text, output, false);
 }
 
 }  // namespace
