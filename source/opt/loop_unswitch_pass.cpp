d558bb0cfe100bace24fdaaf6cc6da4468465ffb
Victor Lomuller <victor@codeplay.com>
2018-02-09 19:14:37 +0000

    Fix build issue with clang

--- source/opt/loop_unswitch_pass.cpp
+++ source/opt/loop_unswitch_pass.cpp
@@ -819,7 +819,9 @@ class LoopUnswitch {
       cfg.RegisterBlock(bb);
     }
 
-    PopulateLoopDesc(new_loop.get(), loop_, {}, {});
+    std::unordered_set<uint32_t> dead_blocks;
+    std::unordered_set<uint32_t> unreachable_merges;
+    PopulateLoopDesc(new_loop.get(), loop_, dead_blocks, unreachable_merges);
 
     return new_loop;
   }
