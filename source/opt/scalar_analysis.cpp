f9fdc6282e7094cbc8df2031f81dd123b18b82c3
Stephen McGroarty <stephen@codeplay.com>
2018-03-20 21:57:11 +0000

    Update in response to feedback.

--- source/opt/scalar_analysis.cpp
+++ source/opt/scalar_analysis.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "opt/scalar_analysis.h"
+
 #include <algorithm>
 #include <functional>
 #include <string>
@@ -20,8 +21,32 @@
 
 #include "opt/ir_context.h"
 
+// Transforms a given scalar operation instruction into a DAG representation.
+//
+// 1. Take an instruction and traverse the its operands until we reach a
+// constant node or any instruction which we do not know how to compute the
+// value of, such as a load.
+//
+// 2. Create a new node for each instruction traversed and build the nodes for
+// the in operands of that instruction as well.
+//
+// 3. Add the operand nodes as children of the first and hash the node. Use the
+// hash to see if the node is already in the cache. We ensure the children are
+// always in sorted order so that two nodes with the same children but inserted
+// in a different order have the same hash and so that the overloaded operator==
+// will return true. If the node is already in the cache return the cached
+// version instead.
+//
+// 4. The created DAG can then be simplified by
+// ScalarAnalysis::SimplifyExpression, implemented in
+// scalar_analysis_simplification.cpp. See that file for further information on
+// the simplification process.
+//
+
 namespace spvtools {
 namespace opt {
+
+uint32_t SENode::NumberOfNodes = 0;
 
 SENode* ScalarEvolutionAnalysis::CreateNegation(SENode* operand) {
   if (operand->GetType() == SENode::Constant) {
@@ -39,6 +64,8 @@ SENode* ScalarEvolutionAnalysis::CreateConstant(int64_t integer) {
 
 SENode* ScalarEvolutionAnalysis::AnalyzeMultiplyOp(
     const ir::Instruction* multiply) {
+  assert(multiply->opcode() == SpvOp::SpvOpIMul &&
+         "Multiply node did not come from a multiply instruction");
   opt::analysis::DefUseManager* def_use = context_->get_def_use_mgr();
 
   SENode* op1 =
@@ -57,21 +84,36 @@ SENode* ScalarEvolutionAnalysis::CreateMultiplyNode(SENode* operand_1,
                           operand_2->AsSEConstantNode()->FoldToSingleValue());
   }
 
-  std::unique_ptr<SENode> add_node{new SEMultiplyNode(this)};
+  std::unique_ptr<SENode> multiply_node{new SEMultiplyNode(this)};
 
-  add_node->AddChild(operand_1);
-  add_node->AddChild(operand_2);
+  multiply_node->AddChild(operand_1);
+  multiply_node->AddChild(operand_2);
 
-  return GetCachedOrAdd(std::move(add_node));
+  return GetCachedOrAdd(std::move(multiply_node));
 }
 
 SENode* ScalarEvolutionAnalysis::CreateSubtraction(SENode* operand_1,
                                                    SENode* operand_2) {
+  // Fold if both operands are constant.
+  if (operand_1->GetType() == SENode::Constant &&
+      operand_2->GetType() == SENode::Constant) {
+    return CreateConstant(operand_1->AsSEConstantNode()->FoldToSingleValue() -
+                          operand_2->AsSEConstantNode()->FoldToSingleValue());
+  }
+
   return CreateAddNode(operand_1, CreateNegation(operand_2));
 }
 
 SENode* ScalarEvolutionAnalysis::CreateAddNode(SENode* operand_1,
-                                               SENode* operand_2) {
+                                               SENode* operand_2,
+                                               bool simplify) {
+  // Fold if both operands are constant and the |simplify| flag is true.
+  if (operand_1->GetType() == SENode::Constant &&
+      operand_2->GetType() == SENode::Constant && simplify) {
+    return CreateConstant(operand_1->AsSEConstantNode()->FoldToSingleValue() +
+                          operand_2->AsSEConstantNode()->FoldToSingleValue());
+  }
+
   std::unique_ptr<SENode> add_node{new SEAddNode(this)};
 
   add_node->AddChild(operand_1);
@@ -91,16 +133,14 @@ SENode* ScalarEvolutionAnalysis::AnalyzeInstruction(
       output = AnalyzePhiInstruction(inst);
       break;
     }
-    case SpvOp::SpvOpConstant: {
+    case SpvOp::SpvOpConstant:
+    case SpvOp::SpvOpConstantNull: {
       output = AnalyzeConstant(inst);
       break;
     }
+    case SpvOp::SpvOpISub:
     case SpvOp::SpvOpIAdd: {
-      output = AnalyzeAddOp(inst, false);
-      break;
-    }
-    case SpvOp::SpvOpISub: {
-      output = AnalyzeAddOp(inst, true);
+      output = AnalyzeAddOp(inst);
       break;
     }
     case SpvOp::SpvOpIMul: {
@@ -117,9 +157,9 @@ SENode* ScalarEvolutionAnalysis::AnalyzeInstruction(
 }
 
 SENode* ScalarEvolutionAnalysis::AnalyzeConstant(const ir::Instruction* inst) {
-  if (inst->NumInOperands() != 1) {
-    assert(false);
-  }
+  assert(inst->NumInOperands() == 1);
+
+  if (inst->opcode() == SpvOp::SpvOpConstantNull) return CreateConstant(0);
 
   int64_t value = 0;
 
@@ -127,10 +167,18 @@ SENode* ScalarEvolutionAnalysis::AnalyzeConstant(const ir::Instruction* inst) {
   const opt::analysis::Constant* constant =
       context_->get_constant_mgr()->FindDeclaredConstant(inst->result_id());
 
-  if (constant->AsIntConstant()->type()->AsInteger()->IsSigned()) {
-    value = constant->AsIntConstant()->GetS32BitValue();
+  if (!constant) return CreateCantComputeNode();
+
+  const opt::analysis::IntConstant* int_constant = constant->AsIntConstant();
+
+  // Exit out if it is a 64 bit integer.
+  if (!int_constant || int_constant->words().size() != 1)
+    return CreateCantComputeNode();
+
+  if (int_constant->type()->AsInteger()->IsSigned()) {
+    value = int_constant->GetS32BitValue();
   } else {
-    value = constant->AsIntConstant()->GetU32BitValue();
+    value = int_constant->GetU32BitValue();
   }
 
   return CreateConstant(value);
@@ -138,28 +186,35 @@ SENode* ScalarEvolutionAnalysis::AnalyzeConstant(const ir::Instruction* inst) {
 
 // Handles both addition and subtraction. If the |sub| flag is set then the
 // addition will be op1+(-op2) otherwise op1+op2.
-SENode* ScalarEvolutionAnalysis::AnalyzeAddOp(const ir::Instruction* add,
-                                              bool sub) {
+SENode* ScalarEvolutionAnalysis::AnalyzeAddOp(const ir::Instruction* inst) {
+  assert((inst->opcode() == SpvOp::SpvOpIAdd or
+          inst->opcode() == SpvOp::SpvOpISub) &&
+         "Add node must be created from a OpIAdd or OpISub instruction");
+
   opt::analysis::DefUseManager* def_use = context_->get_def_use_mgr();
   std::unique_ptr<SENode> add_node{new SEAddNode(this)};
 
-  add_node->AddChild(
-      AnalyzeInstruction(def_use->GetDef(add->GetSingleWordInOperand(0))));
+  SENode* op1 =
+      AnalyzeInstruction(def_use->GetDef(inst->GetSingleWordInOperand(0)));
 
   SENode* op2 =
-      AnalyzeInstruction(def_use->GetDef(add->GetSingleWordInOperand(1)));
+      AnalyzeInstruction(def_use->GetDef(inst->GetSingleWordInOperand(1)));
 
   // To handle subtraction we wrap the second operand in a unary negation node.
-  if (sub) {
+  if (inst->opcode() == SpvOp::SpvOpISub) {
     op2 = CreateNegation(op2);
   }
-  add_node->AddChild(op2);
 
-  return GetCachedOrAdd(std::move(add_node));
+  return CreateAddNode(op1, op2);
 }
 
 SENode* ScalarEvolutionAnalysis::AnalyzePhiInstruction(
     const ir::Instruction* phi) {
+  // The phi should only have two incoming value pairs.
+  if (phi->NumInOperands() != 4) {
+    return CreateCantComputeNode();
+  }
+
   opt::analysis::DefUseManager* def_use = context_->get_def_use_mgr();
 
   // Get the basic block this instruction belongs to.
@@ -177,7 +232,11 @@ SENode* ScalarEvolutionAnalysis::AnalyzePhiInstruction(
 
   // Get the innermost loop which this block belongs to.
   ir::Loop* loop = (*loop_descriptor)[basic_block->id()];
-  if (!loop) return CreateCantComputeNode();
+
+  // If the loop doesn't exist or doesn't have a preheader or latch block, exit
+  // out.
+  if (!loop || !loop->GetLatchBlock() || !loop->GetPreHeaderBlock())
+    return CreateCantComputeNode();
 
   std::unique_ptr<SERecurrentNode> phi_node{new SERecurrentNode(this, loop)};
   instruction_map_[phi] = phi_node.get();
@@ -189,25 +248,22 @@ SENode* ScalarEvolutionAnalysis::AnalyzePhiInstruction(
     ir::Instruction* value_inst = def_use->GetDef(value_id);
     SENode* value_node = AnalyzeInstruction(value_inst);
 
-    // Both operands must be loop invariant.
-    if (!IsLoopInvariant(loop, value_node)) {
-      return CreateCantComputeNode();
-    }
-
     if (incoming_label_id == loop->GetPreHeaderBlock()->id()) {
       phi_node->AddOffset(value_node);
     } else if (incoming_label_id == loop->GetLatchBlock()->id()) {
       // Assumed to be in the form of step + phi.
-      SENode* constant_node = nullptr;
+      if (value_node->GetType() != SENode::Add) return CreateCantComputeNode();
+
+      SENode* step_node = nullptr;
       SENode* phi_operand = nullptr;
       SENode* operand_1 = value_node->GetChild(0);
       SENode* operand_2 = value_node->GetChild(1);
 
       // Find which node is the step term.
       if (!operand_1->AsSERecurrentNode())
-        constant_node = operand_1;
+        step_node = operand_1;
       else if (!operand_2->AsSERecurrentNode())
-        constant_node = operand_2;
+        step_node = operand_2;
 
       // Find which node is the recurrent expression.
       if (operand_1->AsSERecurrentNode())
@@ -216,12 +272,16 @@ SENode* ScalarEvolutionAnalysis::AnalyzePhiInstruction(
         phi_operand = operand_2;
 
       // If it is not in the form step + phi exit out.
-      if (!(constant_node && phi_operand)) return CreateCantComputeNode();
+      if (!(step_node && phi_operand)) return CreateCantComputeNode();
 
       // If the phi operand is not the same phi node exit out.
       if (phi_operand != phi_node.get()) return CreateCantComputeNode();
 
-      phi_node->AddCoefficient(constant_node);
+      if (!IsLoopInvariant(loop, step_node)) {
+        return CreateCantComputeNode();
+      }
+
+      phi_node->AddCoefficient(step_node);
     }
   }
 
@@ -261,15 +321,15 @@ bool ScalarEvolutionAnalysis::IsLoopInvariant(const ir::Loop* loop,
     if (const SERecurrentNode* rec = itr->AsSERecurrentNode()) {
       const ir::BasicBlock* header = rec->GetLoop()->GetHeaderBlock();
 
-      // If the header block is not the same as |loop| header block or is not
-      // inside the loop then assume invariance.
-      if (header != loop->GetHeaderBlock() && !loop->IsInsideLoop(header))
+      // If the loop which the recurrent expression belongs to is either |loop|
+      // or a nested loop inside |loop| then we assume it is variant.
+      if (loop->IsInsideLoop(header)) {
         return false;
-    }
-    if (const SEValueUnknown* unknown = itr->AsSEValueUnknown()) {
+      }
+    } else if (const SEValueUnknown* unknown = itr->AsSEValueUnknown()) {
       // If the instruction is inside the loop we conservatively assume it is
       // loop variant.
-      if (loop->IsInsideLoop(unknown->UniqueId())) return false;
+      if (loop->IsInsideLoop(unknown->ResultId())) return false;
     }
   }
 
@@ -280,8 +340,8 @@ std::string SENode::AsString() const {
   switch (GetType()) {
     case Constant:
       return "Constant";
-    case RecurrentExpr:
-      return "RecurrentExpr";
+    case RecurrentAddExpr:
+      return "RecurrentAddExpr";
     case Add:
       return "Add";
     case Negative:
@@ -308,8 +368,8 @@ bool SENode::operator==(const SENode& other) const {
   // If we're dealing with a value unknown node check both nodes were created by
   // the same instruction.
   if (GetType() == SENode::ValueUnknown) {
-    if (AsSEValueUnknown()->UniqueId() !=
-        other.AsSEValueUnknown()->UniqueId()) {
+    if (AsSEValueUnknown()->ResultId() !=
+        other.AsSEValueUnknown()->ResultId()) {
       return false;
     }
   }
@@ -325,42 +385,66 @@ bool SENode::operator==(const SENode& other) const {
 
 bool SENode::operator!=(const SENode& other) const { return !(*this == other); }
 
+namespace {
+// Helper functions to insert 32/64 bit values into the 32 bit hash string. This
+// allows us to add pointers to the string by reinterpreting thie pointers as
+// uintptr_t which will result in them going to either the uint32_t or uint64_t
+// overload depending on the platform.
+
+static void PushToString(uint32_t id, std::u32string* str) {
+  str->push_back(id);
+}
+
+static void PushToString(uint64_t id, std::u32string* str) {
+  str->push_back(static_cast<uint32_t>(id << 32));
+  str->push_back(static_cast<uint32_t>(id));
+}
+
+static void PushToString(int64_t id, std::u32string* str) {
+  str->push_back(static_cast<uint32_t>(id << 32));
+  str->push_back(static_cast<uint32_t>(id));
+}
+
+}  // namespace
+
 // Implements the hashing of SENodes.
 size_t SENodeHash::operator()(const SENode* node) const {
+  // Concatinate the terms into a string which we can hash.
+  std::u32string hash_string{};
+
   // Hashing the type as a string is safer than hashing the enum as the enum is
   // very likely to collide with constants.
-  std::string type = node->AsString();
+  for (char ch : node->AsString()) {
+    hash_string.push_back(static_cast<char32_t>(ch));
+  }
 
   // We just ignore the literal value unless it is a constant.
-  int64_t literal_value = 0;
   if (node->GetType() == SENode::Constant)
-    literal_value = node->AsSEConstantNode()->FoldToSingleValue();
-
-  // Hash the type string and the constant value if any.
-  size_t resulting_hash =
-      std::hash<std::string>{}(type) ^ std::hash<int64_t>{}(literal_value);
+    PushToString(node->AsSEConstantNode()->FoldToSingleValue(), &hash_string);
 
   // If we're dealing with a recurrent expression hash the loop as well so that
   // nested inductions like i=0,i++ and j=0,j++ correspond to different nodes.
-  if (node->GetType() == SENode::RecurrentExpr) {
-    resulting_hash ^=
-        std::hash<const ir::Loop*>{}(node->AsSERecurrentNode()->GetLoop());
+  if (node->GetType() == SENode::RecurrentAddExpr) {
+    PushToString(
+        reinterpret_cast<uintptr_t>(node->AsSERecurrentNode()->GetLoop()),
+        &hash_string);
   }
 
-  // Hash the unique id of the creator instruction if this is a value unknown
+  // Hash the result id of the original instruction which created this node if
+  // it is a value unknown
   // node.
   if (node->GetType() == SENode::ValueUnknown) {
-    resulting_hash ^=
-        std::hash<uint32_t>{}(node->AsSEValueUnknown()->UniqueId());
+    PushToString(node->AsSEValueUnknown()->ResultId(), &hash_string);
   }
+
   // Hash the pointers of the child nodes, each SENode has a unique pointer
   // associated with it.
   const std::vector<SENode*>& children = node->GetChildren();
   for (const SENode* child : children) {
-    resulting_hash ^= std::hash<const SENode*>{}(child);
+    PushToString(reinterpret_cast<uintptr_t>(child), &hash_string);
   }
 
-  return resulting_hash;
+  return std::hash<std::u32string>{}(hash_string);
 }
 
 // This overload is the actual overload used by the node_cache_ set.
